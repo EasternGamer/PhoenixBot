2a1430e486da7259ddcfbf7c9a5a22313f9932dd
EasternGamer <crysishunter32@gmail.com>
2018-04-27 18:56:38 +0200

    Update UpdateCommand.java

--- src/MainBot/UpdateCommand.java
+++ src/MainBot/UpdateCommand.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2018 crysi
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package MainBot;
 
 import java.sql.Connection;
@@ -28,6 +12,7 @@ import java.util.logging.Logger;
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IRole;
 import sx.blah.discord.handle.obj.IUser;
+import sx.blah.discord.util.RequestBuffer;
 
 /**
  *
@@ -81,7 +66,9 @@ public class UpdateCommand {
                     for (IUser player : players) {
                         if (!player.getNicknameForGuild(guild).equals(rank + player.getName())) {
                             if (player.hasRole(r)) {
+                                RequestBuffer.request(() -> {
                                 guild.setUserNickname(player, rank + player.getName());
+                                });
                             }
                         }
                     }

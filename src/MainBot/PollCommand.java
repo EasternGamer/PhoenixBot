2cfb8672ce54302078e6bb8e660504fc57442e9c
EasternGamer <crysishunter32@gmail.com>
2018-04-27 18:56:03 +0200

    Update PollCommand.java

--- src/MainBot/PollCommand.java
+++ src/MainBot/PollCommand.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2018 crysi
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package MainBot;
 
 import static MainBot.JRASBotGUI.NEWLINE;
@@ -22,6 +6,7 @@ import java.util.List;
 import sx.blah.discord.handle.impl.obj.ReactionEmoji;
 import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.util.EmbedBuilder;
+import sx.blah.discord.util.RequestBuffer;
 
 /**
  *
@@ -200,6 +185,7 @@ public class PollCommand {
             String seven = "\u0037\u20E3";
             String eight = "\u0038\u20E3";
             String nine = "\u0039\u20E3";
+            RequestBuffer.request(() -> {
             if (option1B == true) {
                 latest.addReaction(ReactionEmoji.of(one));
             }
@@ -237,7 +223,8 @@ public class PollCommand {
             option7B = false;
             option8B = false;
             option9B = false;
-        }
+        });
+      }
     }
 
 }
